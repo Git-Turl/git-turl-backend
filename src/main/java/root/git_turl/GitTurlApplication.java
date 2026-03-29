c4a38199e27acc85f1b322b580fd57720e8e35bb
seoyoon lee <leeseoyoon01@gmail.com>
2026-03-29 20:22:53 +0900

    chore: 초기세팅

---
+++ src/main/java/root/git_turl/GitTurlApplication.java
@@ -0,0 +1,13 @@
+package root.git_turl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTurlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/root/git_turl/GitTurlApplicationTests.java
@@ -0,0 +1,13 @@
+package root.git_turl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
