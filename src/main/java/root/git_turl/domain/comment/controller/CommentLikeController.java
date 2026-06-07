5c8024dda9dcf0c8c315c1553c1617834b4e026b
ywkim1m <ywkim1m@gmail.com>
2026-06-07 21:28:39 +0900

    feat: 댓글 좋아요 응답 필드 추가

--- src/main/java/root/git_turl/domain/comment/controller/CommentLikeController.java
+++ src/main/java/root/git_turl/domain/comment/controller/CommentLikeController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import root.git_turl.domain.comment.code.CommentSuccessCode;
+import root.git_turl.domain.comment.dto.CommentResDto;
 import root.git_turl.domain.comment.service.CommentLikeCommandService;
 import root.git_turl.domain.member.entity.Member;
 import root.git_turl.domain.member.repository.MemberRepository;
@@ -19,33 +20,33 @@ public class CommentLikeController implements CommentLikeControllerDocs {
     private final CommentLikeCommandService commentLikeCommandService;
 
 
-    @PostMapping("/comments/{commentId}/likes")
-    public ApiResponse<Void> likeComment(
+    @PostMapping("/{commentId}/likes")
+    public ApiResponse<CommentResDto.CommentLikeResultDto> likeComment(
             @CurrentUser Member member,
             @PathVariable Long commentId
     ) {
 
-
-        commentLikeCommandService.likeComment(member, commentId);
+        CommentResDto.CommentLikeResultDto result =
+                commentLikeCommandService.likeComment(member, commentId);
 
         return ApiResponse.onSuccess(
                 CommentSuccessCode.COMMENT_LIKE_CREATED,
-                null
+                result
         );
     }
 
-    @DeleteMapping("/comments/{commentId}/likes")
-    public ApiResponse<Void> unlikeComment(
+    @DeleteMapping("/{commentId}/likes")
+    public ApiResponse<CommentResDto.CommentLikeResultDto> unlikeComment(
             @CurrentUser Member member,
             @PathVariable Long commentId
     ) {
 
-
-        commentLikeCommandService.unlikeComment(member, commentId);
+        CommentResDto.CommentLikeResultDto result =
+                commentLikeCommandService.unlikeComment(member, commentId);
 
         return ApiResponse.onSuccess(
                 CommentSuccessCode.COMMENT_LIKE_DELETED,
-                null
+                result
         );
     }
 }
