23c279d846dda0a1c57df3330abafe97b56d40e3
ywkim1m <ywkim1m@gmail.com>
2026-06-10 00:24:18 +0900

    fix/관심 스택 접근 방식 변경

--- src/main/java/root/git_turl/domain/board/repository/BoardRecommendInterestStackRepository.java
+++ src/main/java/root/git_turl/domain/board/repository/BoardRecommendInterestStackRepository.java
@@ -1,4 +1,12 @@
 package root.git_turl.domain.board.repository;
 
-public interface BoardRecommendInterestStackRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import root.git_turl.domain.member.entity.InterestStack;
+
+import java.util.List;
+
+public interface BoardRecommendInterestStackRepository
+        extends JpaRepository<InterestStack, Long> {
+
+    List<InterestStack> findAllByMemberId(Long memberId);
 }
