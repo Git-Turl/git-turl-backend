0decb31d9967f06acff66873d116f9da984e1182
seoyoon lee <leeseoyoon01@gmail.com>
2026-07-16 22:58:06 +0900

    fix: remove secrets

--- src/main/java/root/git_turl/infrastructure/openai/GptService.java
+++ src/main/java/root/git_turl/infrastructure/openai/GptService.java
@@ -1,105 +1,36 @@
 package root.git_turl.infrastructure.openai;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
-import root.git_turl.domain.answer.dto.AnswerResDto;
 import root.git_turl.domain.answer.dto.Feedback;
 import root.git_turl.domain.answer.dto.VoiceFeedback;
 import root.git_turl.domain.question.dto.QuestionContent;
-import root.git_turl.domain.report.dto.Problem;
 import root.git_turl.domain.report.dto.ProblemList;
-import root.git_turl.domain.report.dto.gpt.GptMessage;
-import root.git_turl.domain.report.dto.gpt.GptRequest;
-import root.git_turl.domain.report.dto.gpt.GptResponse;
 import root.git_turl.domain.report.dto.reportDetail.ReportWrapper;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class GptService {
-    private final RestTemplate restTemplate;
-    private final ObjectMapper objectMapper;
-
-    private final String OPENAI_API = "https://api.openai.com/v1";
-
-    @Value("${spring.ai.openai.api-key}")
-    public String openAiApiKey;
+    private final RequestGpt requestGpt;
 
     public ReportWrapper analyzeGit(String prompt) {
-        return requestGpt(prompt, ReportWrapper.class);
+        return requestGpt.requestGpt(prompt, ReportWrapper.class);
     }
 
     public QuestionContent makeQuestions(String prompt) {
-        return requestGpt(prompt, QuestionContent.class);
+        return requestGpt.requestGpt(prompt, QuestionContent.class);
     }
 
     public Feedback makeFeedback(String prompt) {
-        return requestGpt(prompt, Feedback.class);
+        return requestGpt.requestGpt(prompt, Feedback.class);
     }
 
     public VoiceFeedback makeVoiceFeedback(String prompt) {
-        return requestGpt(prompt, VoiceFeedback.class);
+        return requestGpt.requestGpt(prompt, VoiceFeedback.class);
     }
 
-    public ProblemList makeReportProblem(String prompt) {return requestGpt(prompt, ProblemList.class);}
+    public ProblemList makeReportProblem(String prompt) {return requestGpt.requestGpt(prompt, ProblemList.class);}
 
-    private <T> T requestGpt(String prompt, Class<T> classType) {
-        GptRequest request = new GptRequest();
-        request.setMessages(List.of(
-                GptMessage.builder()
-                        .role("user")
-                        .content(prompt)
-                        .build()
-        ));
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setBearerAuth(openAiApiKey);
-
-        HttpEntity<GptRequest> entity = new HttpEntity<>(request, headers);
-
-        ResponseEntity<GptResponse> response =
-                restTemplate.postForEntity(
-                        OPENAI_API + "/chat/completions",
-                        entity,
-                        GptResponse.class
-                );
-
-        String json = response.getBody().getChoices().get(0).getMessage().getContent().trim();
-        if (json.startsWith("```")) {
-            json = json.replaceFirst("^```(?:json)?\\s*", "");
-            json = json.replaceFirst("\\s*```$", "");
-        }
-
-        try {
-            objectMapper.readTree(json);  // JSON 문법 검사
-            return objectMapper.readValue(json, classType);
-        } catch (JsonProcessingException e) {
-            try {
-                Files.writeString(
-                        Path.of("failed-json-" + System.currentTimeMillis() + ".json"),
-                        json
-                );
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-
-            log.error("JSON parse failed", e);
-            throw new RuntimeException("GPT 응답 파싱 실패: " + json, e);
-        }
-    }
 }
